db72fb699e18cecefe82c13167cdefcf6d16decc
zhangxuhui <11697786188@qq.com>
2023-05-16 20:34:02 +0800

    完成了简易的tcp server和tcp client

--- EasyTcpClient/client.cpp
+++ EasyTcpClient/client.cpp
@@ -1,14 +1,69 @@
-#define WIN32_LEAN_AND_MEAN
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#ifdef _WIN32
+	#define WIN32_LEAN_AND_MEAN
+	#define _WINSOCK_DEPRECATED_NO_WARNINGS
+	#include <windows.h>
+	#include <WinSock2.h>
+#else
+	#include <unistd.h>
+	#include <arpa/inet.h>
+	#include <string.h>
 
-#include <windows.h>
-#include <WinSock2.h>
+	#define SOCKET int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif
+
 #include <stdio.h>
+#include <thread>
+
+bool g_bRun = true;
+
+void CMD_Task(SOCKET conn_fd) {
+	while (true) {
+		char cmdBuf[256] = {};
+#ifdef _WIN32
+		scanf_s("%s", cmdBuf, 256);
+#else 
+		scanf("%s", cmdBuf);
+#endif
+		if (0 == strcmp(cmdBuf, "exit")) {
+			g_bRun = false;
+			printf("client is over...\n");
+			return;
+		}
+		else if (0 == strcmp(cmdBuf, "getname")) {
+			char sendBuf[] = "getname";
+			send(conn_fd, sendBuf, strlen(sendBuf) + 1, 0);
+		}
+		else if (0 == strcmp(cmdBuf, "getage")) {
+			char sendBuf[] = "getage";
+			send(conn_fd, sendBuf, strlen(sendBuf) + 1, 0);
+		}
+		else {
+			printf("%s\n", cmdBuf);
+			printf("unsupported command\n");
+			char sendBuf[] = "unsupported command";
+			send(conn_fd, sendBuf, strlen(sendBuf) + 1, 0);
+		}
+	}
+}
+
+int processor(SOCKET conn_fd) {
+	char buf[256] = {};
+	int len = recv(conn_fd, buf, 256, 0);
+	if (len > 0) {
+		printf("recv data, the data is : %s\n", buf);
+		return 0;
+	}
+	return -1;
+}
 
 int main() {
+#ifdef _WIN32
 	WORD ver = MAKEWORD(2, 2);
 	WSADATA data;
 	WSAStartup(ver, &data);
+#endif
 	//---------------------
 	SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (INVALID_SOCKET == _sock) {
@@ -20,21 +75,52 @@ int main() {
 	sockaddr_in serv_addr = {};
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(9527);
-	serv_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+#ifdef _WIN32
+	serv_addr.sin_addr.S_un.S_addr = inet_addr("192.168.145.130");
+#else
+	serv_addr.sin_addr.s_addr = inet_addr("192.168.145.1");
+#endif
 	if (SOCKET_ERROR == connect(_sock, (sockaddr*)&serv_addr, sizeof(serv_addr))) {
 		printf("connect error");
 	}
 	else {
 		printf("connect success");
 	}
-	char buf[256] = {};
-	int len = recv(_sock, buf, 256, 0);
-	if (len > 0) {
-		printf("recv data, the data is : %s\n", buf);
+
+	std::thread cmd_thread(CMD_Task, _sock);
+	cmd_thread.detach();
+
+	while (g_bRun)
+	{
+		fd_set read_fds;
+
+		FD_ZERO(&read_fds);
+
+		FD_SET(_sock, &read_fds);
+		timeval val = { 1, 0 };
+
+		if (select(_sock + 1, &read_fds, NULL, NULL, &val) < 0) {
+			printf("client select is over...\n");
+			break;
+		}
+
+		if (FD_ISSET(_sock, &read_fds)) {
+			FD_CLR(_sock, &read_fds);
+
+			if (-1 == processor(_sock)) {
+				printf("the connection occurs error...\n");
+				break;
+			}
+		}
 	}
+#ifdef _WIN32	
 	closesocket(_sock);
+#else
+	close(_sock);
+#endif
 	//---------------------
+#ifdef _WIN32
 	WSACleanup();
-	getchar();
+#endif
 	return 0;
 }
--- EasyTcpServer/server.cpp
+++ EasyTcpServer/server.cpp
@@ -1,57 +1,125 @@
-#define WIN32_LEAN_AND_MEAN
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#ifdef _WIN32
+	#define WIN32_LEAN_AND_MEAN
+	#define _WINSOCK_DEPRECATED_NO_WARNINGS
+	#include <windows.h>
+	#include <WinSock2.h>
+#else
+	#include <unistd.h>
+	#include <arpa/inet.h>
+	#include <string.h>
 
-#include <windows.h>
-#include <WinSock2.h>
+	#define SOCKET int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif
 #include <stdio.h>
+#include <thread>
+
+bool g_bRun = true;
+
+void CMD_Task(SOCKET conn_fd) {
+	while (true) {
+		char cmdBuf[256] = {};
+#ifdef _WIN32
+		scanf_s("%s", cmdBuf, 256);
+#else 
+		scanf("%s", cmdBuf);
+#endif
+		if (0 == strcmp(cmdBuf, "exit")) {
+			g_bRun = false;
+			printf("client is over...\n");
+			return;
+		}
+		else if (0 == strcmp(cmdBuf, "getname")) {
+			char sendBuf[] = "getname";
+			send(conn_fd, sendBuf, strlen(sendBuf) + 1, 0);
+		}
+		else if (0 == strcmp(cmdBuf, "getage")) {
+			char sendBuf[] = "getage";
+			send(conn_fd, sendBuf, strlen(sendBuf) + 1, 0);
+		}
+		else {
+			printf("%s\n", cmdBuf);
+			printf("unsupported command\n");
+			char sendBuf[] = "unsupported command";
+			send(conn_fd, sendBuf, strlen(sendBuf) + 1, 0);
+		}
+	}
+}
+
+int processor(SOCKET conn_fd) {
+	char buf[256] = {};
+	int len = recv(conn_fd, buf, 256, 0);
+	if (len > 0) {
+		printf("recv data, the data is : %s\n", buf);
+		return 0;
+	}
+	return -1;
+}
 
 int main() {
+#ifdef _WIN32
 	WORD ver = MAKEWORD(2, 2);
 	WSADATA data;
 	WSAStartup(ver, &data);
+#endif
 	//---------------------
 	SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (INVALID_SOCKET == _sock) {
-		printf("server create socket error\n");
+		printf("client create socket error\n");
 	}
 	else {
-		printf("server create socket successfully\n");
+		printf("client create socket success\n");
 	}
 	sockaddr_in serv_addr = {};
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(9527);
-	serv_addr.sin_addr.S_un.S_addr = INADDR_ANY;
-
-	if (SOCKET_ERROR == bind(_sock, (sockaddr*)&serv_addr, sizeof(serv_addr))) {
-		printf("server bind socket error\n");
+#ifdef _WIN32
+	serv_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+#else
+	serv_addr.sin_addr.s_addr = inet_addr("192.168.145.130");
+#endif
+	if (SOCKET_ERROR == connect(_sock, (sockaddr*)&serv_addr, sizeof(serv_addr))) {
+		printf("connect error");
 	}
 	else {
-		printf("server bind socket successfully\n");
+		printf("connect success");
 	}
 
-	if (SOCKET_ERROR == listen(_sock, 5)) {
-		printf("server listen socket error\n");
-	}
-	else {
-		printf("server listen socket successfully\n");
-	}
+	std::thread cmd_thread(CMD_Task, _sock);
+	cmd_thread.detach();
 
-	sockaddr_in clit_addr = {};
-	int clit_addr_len = sizeof(clit_addr);
-	while (true) {
-		SOCKET _cSock = accept(_sock, (sockaddr*)&clit_addr, &clit_addr_len);
-		if (INVALID_SOCKET == _cSock) {
-			printf("accept error");
+	while (g_bRun)
+	{
+		fd_set read_fds;
+
+		FD_ZERO(&read_fds);
+
+		FD_SET(_sock, &read_fds);
+		timeval val = { 1, 0 };
+
+		if (select(_sock + 1, &read_fds, NULL, NULL, &val) < 0) {
+			printf("client select is over...\n");
+			break;
 		}
-		else {
-			printf("accept success, the new client is %s\n", inet_ntoa(clit_addr.sin_addr));
-		}
 
-		char buf[] = "hello, i am the server";
-		send(_cSock, buf, strlen(buf) + 1, 0);
+		if (FD_ISSET(_sock, &read_fds)) {
+			FD_CLR(_sock, &read_fds);
+
+			if (-1 == processor(_sock)) {
+				printf("the connection occurs error...\n");
+				break;
+			}
+		}
 	}
+#ifdef _WIN32	
 	closesocket(_sock);
-	//-------------------
+#else
+	close(_sock);
+#endif
+	//---------------------
+#ifdef _WIN32
 	WSACleanup();
+#endif
 	return 0;
 }
